d5a82baf51d10a722ecf86cadb7f3a6951719bed
Iamneo <techadmin@iamneo.ai>
2023-08-11 11:18:38 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,36 +1,36 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
-//import { DOCUMENT } from "@angular/common";
+import { FormBuilder } from "@angular/forms";
+
 @Component({
-selector: 'app-root',
-templateUrl:'./app.component.html',
-styleUrls:['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-resValue: any
-fromCurrency: any
-toCurrency: any
-amount:any
-CurrencyForm :FormGroup; 
-constructor(public fb: FormBuilder) { 
-this.CurrencyForm=this.fb.group({
-fromCurrency: [''],
-amount: [''],
-toCurrency: ['']
- });
-}
-onSubmitt() {
-    if(this.CurrencyForm){
- this.amount=this.CurrencyForm.get('amount')?.value;
- this.fromCurrency=this.CurrencyForm.get('fromCurrency')?.value;
- this.toCurrency=this.CurrencyForm.get('toCurrency')?.value;
- //this.amount=
- //this.resValue='1.00';
- if(this.amount!==null && this.CurrencyForm!==null && this.toCurrency!==null){
- this.resValue=(this.toCurrency/this.fromCurrency)*this.amount;
- this.resValue=Math.round(this.resValue*100)/100;
- //this.resValue=parseFloat(this.resValue).toFixed(2);
- }
-}
- }
+  resValue: any;
+  fromCurrency: any;
+  toCurrency: any;
+  amount: any;
+
+  constructor(public fb: FormBuilder) { }
+
+  CurrencyForm = this.fb.group({
+    fromCurrency: [''],
+    amount: [''],
+    toCurrency: ['']
+  });
+
+  onSubmitt() {
+    this.amount = this.CurrencyForm.get('amount')?.value;
+    this.fromCurrency = this.CurrencyForm.get('fromCurrency')?.value;
+    this.toCurrency = this.CurrencyForm.get('toCurrency')?.value;
+
+    if (this.fromCurrency !== null && this.toCurrency !== null) {
+      this.resValue = (this.amount * this.fromCurrency) / this.toCurrency;
+      this.resValue = parseFloat(this.resValue).toFixed(2);
+    } else {
+      this.resValue = "Invalid input";
+    }
+  }
 }
