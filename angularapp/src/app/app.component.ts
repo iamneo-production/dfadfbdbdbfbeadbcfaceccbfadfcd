4d12da56bb87232cca857397db284a9badbd71b4
Iamneo <techadmin@iamneo.ai>
2023-08-11 10:48:36 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,36 +1,30 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { FormBuilder } from "@angular/forms";
+//import { DOCUMENT } from "@angular/common";
 @Component({
-  selector: 'app-currency',
-  templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class CurrencyComponent {
-  resValue: any = null;
-  fromCurrency: number = 1.126735; // Default to USD
-  toCurrency: number = 1.126735; // Default to USD
-  amount: number = 0;
-  CurrencyForm: FormGroup;
-
-  exchangeRates = {
-    USD: 1.126735,
-    GBP: 0.876893,
-    INR: 79.677056
-  };
-
-  constructor(public fb: FormBuilder) {
-    this.CurrencyForm = this.fb.group({
-      fromCurrency: ['USD'],
-      amount: [''],
-      toCurrency: ['USD']
-    });
-  }
-
-  onSubmitt() {
-    this.amount = this.CurrencyForm.get('amount').value;
-    this.fromCurrency = this.exchangeRates[this.CurrencyForm.get('fromCurrency').value];
-    this.toCurrency = this.exchangeRates[this.CurrencyForm.get('toCurrency').value];
-    this.resValue = ((this.toCurrency / this.fromCurrency) * this.amount).toFixed(2);
-  }
+export class AppComponent {
+  resValue: any
+  fromCurrency: any
+  toCurrency: any
+  amount:any
+  constructor(public fb: FormBuilder) { }
+  CurrencyForm = this.fb.group({
+    fromCurrency: [''],
+    amount: [''],
+    toCurrency: ['']
+  })
+  onSubmitt() {
+    this.amount=this.CurrencyForm.get('amount').value;  
+    this.fromCurrency=this.CurrencyForm.get('fromCurrency').value;
+    this.toCurrency=this.CurrencyForm.get('toCurrency').value;
+    //this.amount=
+    //this.resValue='1.00';
+    this.resValue=(this.toCurrency/this.fromCurrency)*this.amount;
+    this.resValue=Math.round(this.resValue);
+    this.resValue=parseFloat(this.resValue).toFixed(2);
+  }
 }
