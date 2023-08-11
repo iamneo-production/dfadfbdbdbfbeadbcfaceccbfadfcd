b9f6105f129d15d0dc868815108dc8c5c7dbb190
Iamneo <techadmin@iamneo.ai>
2023-08-11 10:38:38 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CurrencyComponent } from './currency/currency.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CurrencyComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    CurrencyComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }
