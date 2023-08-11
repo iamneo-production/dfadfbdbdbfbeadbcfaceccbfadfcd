00dc718e52b7bcefd6f1140882fc28d4d89e7559
Iamneo <techadmin@iamneo.ai>
2023-08-11 11:03:36 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, FormGroup } from "@angular/forms";
 //import { DOCUMENT } from "@angular/common";
 @Component({
 selector: 'app-root',
@@ -11,18 +11,22 @@ resValue: any
 fromCurrency: any
 toCurrency: any
 amount:any
-constructor(public fb: FormBuilder) { }
-CurrencyForm = this.fb.group({
+CurrencyForm :FormGroup; 
+constructor(public fb: FormBuilder) { 
+this.CurrencyForm=this.fb.group({
 fromCurrency: [''],
 amount: [''],
 toCurrency: ['']
- })
+ });
+}
 onSubmitt() {
- this.amount=this.CurrencyForm.get('amount').value;
- this.fromCurrency=this.CurrencyForm.get('fromCurrency').value;
- this.toCurrency=this.CurrencyForm.get('toCurrency').value;
+    if(this.CurrencyForm){
+ this.amount=this.CurrencyForm.get('amount')?.value;
+ this.fromCurrency=this.CurrencyForm.get('fromCurrency')?.value;
+ this.toCurrency=this.CurrencyForm.get('toCurrency')?.value;
  //this.amount=
  //this.resValue='1.00';
+ if(this.amount!==null && this.CurrencyForm!==null)
  this.resValue=(this.toCurrency/this.fromCurrency)*this.amount;
  this.resValue=Math.round(this.resValue);
  this.resValue=parseFloat(this.resValue).toFixed(2);
